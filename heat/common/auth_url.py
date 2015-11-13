578bbd362c9f14a191f712337b6789bc94731b0a
Vincent Untz <vuntz@suse.com>
2015-11-13 02:05:23 +0100

    Fix discovery of keystone auth URI with keystone on SSL
    Change-Id: Iaa22673699b9e23fc521a7fed999de4606e27315
    (cherry picked from commit bd8be353c89faf64304e7e8db253ab05fbac04af)

--- heat/common/auth_url.py
+++ heat/common/auth_url.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 from keystoneclient import discover as ks_discover
+from keystoneclient import session
 from oslo_config import cfg
 from oslo_utils import importutils
 from webob import exc
@@ -28,6 +29,7 @@ class AuthUrlFilter(wsgi.Middleware):
     def __init__(self, app, conf):
         super(AuthUrlFilter, self).__init__(app)
         self.conf = conf
+        self.session = session.Session.construct(self._ssl_options())
         self.auth_url = self._get_auth_url()
 
     def _get_auth_url(self):
@@ -39,6 +41,7 @@ class AuthUrlFilter(wsgi.Middleware):
             # look in [keystone_authtoken]
             if cfg.CONF.clients_keystone.auth_uri:
                 discover = ks_discover.Discover(
+                    self.session,
                     auth_url=cfg.CONF.clients_keystone.auth_uri)
                 return discover.url_for('3.0')
             else:
@@ -66,6 +69,25 @@ class AuthUrlFilter(wsgi.Middleware):
 
         req.headers['X-Auth-Url'] = auth_url
         return None
+
+    def _ssl_options(self):
+        opts = {'cacert': self._get_client_option('ca_file'),
+                'insecure': self._get_client_option('insecure'),
+                'cert': self._get_client_option('cert_file'),
+                'key': self._get_client_option('key_file')}
+        return opts
+
+    def _get_client_option(self, option):
+        # look for the option in the [clients_keystone] section
+        # unknown options raise cfg.NoSuchOptError
+        cfg.CONF.import_opt(option, 'heat.common.config',
+                            group='clients_keystone')
+        v = getattr(cfg.CONF.clients_keystone, option)
+        if v is not None:
+            return v
+        # look for the option in the generic [clients] section
+        cfg.CONF.import_opt(option, 'heat.common.config', group='clients')
+        return getattr(cfg.CONF.clients, option)
 
 
 def filter_factory(global_conf, **local_conf):
